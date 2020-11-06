3d4a463f5b7d33c6c180162c3364d5338632fe49
Requejo <Requejo@PC12>
2020-11-06 19:01:26 +0100

    Nueva aplicacion(Segunda subida) Borro primera clase

--- PrimeraAplicacion/src/primeraaplicacion/Segundaclase.java
+++ PrimeraAplicacion/src/primeraaplicacion/Segundaclase.java
@@ -9,15 +9,6 @@ package primeraaplicacion;
  *
  * @author Requejo
  */
-public class PrimeraAplicacion {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
-        
-    }
+public class Segundaclase {
     
 }
